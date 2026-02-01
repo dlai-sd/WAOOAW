b460b8fecee2b4d7d056346ec1dfc1e479fdc876
Deep Learning AI Satellite Data <yogeshk7377@gmail.com>
2026-02-01 13:57:27 +0000

    fix(terraform): fmt plant stack

--- cloud/terraform/stacks/plant/main.tf
+++ cloud/terraform/stacks/plant/main.tf
@@ -99,7 +99,7 @@ module "plant_backend" {
     {
       DATABASE_URL = "${module.plant_database.database_url_secret_id}:latest"
     }
-    ) : {
+  ) : {
     DATABASE_URL = "${module.plant_database.database_url_secret_id}:latest"
   }
 
@@ -141,8 +141,8 @@ module "plant_gateway" {
     PLANT_BACKEND_URL         = module.plant_backend.service_url
     PLANT_BACKEND_USE_ID_TOKEN = "true"
     PLANT_BACKEND_AUDIENCE     = module.plant_backend.service_url
-    OPA_SERVICE_URL           = "https://opa-policy-engine.a.run.app" # TODO: Create OPA service
-    REDIS_HOST                = "10.0.0.3"                            # TODO: Create Redis instance
+    OPA_SERVICE_URL            = "https://opa-policy-engine.a.run.app" # TODO: Create OPA service
+    REDIS_HOST                 = "10.0.0.3"                            # TODO: Create Redis instance
     CLOUD_SQL_CONNECTION_NAME = module.plant_database.instance_connection_name
     JWT_ISSUER                = "waooaw.com"
     JWT_ALGORITHM             = "HS256"
@@ -152,7 +152,7 @@ module "plant_gateway" {
     DATABASE_URL   = "${module.plant_database.database_url_secret_id}:latest"
     JWT_SECRET     = "JWT_SECRET:latest"
     JWT_PUBLIC_KEY = "JWT_SECRET:latest"
-    } : {
+  } : {
     DATABASE_URL = "${module.plant_database.database_url_secret_id}:latest"
   }
 
