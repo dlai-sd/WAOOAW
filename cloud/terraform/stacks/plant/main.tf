964345532c74911bcf153bacebd8ec3e190cfe26
Deep Learning AI Satellite Data <yogeshk7377@gmail.com>
2026-02-01 13:44:46 +0000

    fix(demo): gateway ID token + show DB updates 401

--- cloud/terraform/stacks/plant/main.tf
+++ cloud/terraform/stacks/plant/main.tf
@@ -139,6 +139,8 @@ module "plant_gateway" {
   env_vars = {
     ENVIRONMENT               = var.environment
     PLANT_BACKEND_URL         = module.plant_backend.service_url
+    PLANT_BACKEND_USE_ID_TOKEN = "true"
+    PLANT_BACKEND_AUDIENCE     = module.plant_backend.service_url
     OPA_SERVICE_URL           = "https://opa-policy-engine.a.run.app" # TODO: Create OPA service
     REDIS_HOST                = "10.0.0.3"                            # TODO: Create Redis instance
     CLOUD_SQL_CONNECTION_NAME = module.plant_database.instance_connection_name
